04d9998a970d2fd290c5d54ec663404cf432779e
JeffRomance <maxi_johansen@hotmail.com>
2026-03-17 12:50:10 +0100

    orderline klasse

--- src/OrderLine.java
+++ src/OrderLine.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class OrderLine {
+    private int quantity;
+    private Pizza pizza;
+
+    public OrderLine(int quantity, Pizza pizza){
+        this.quantity = quantity;
+        this.pizza = pizza;
+    }
+    public double getTotal(){
+        return pizza.getPrice() * quantity;
+    }
+    public String toString(){
+        return String.format("%s %d %d %2f", this.pizza.getName(), this.quantity, pizza.getPrice(), getTotal());
+    }
 }
