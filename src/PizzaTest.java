c4b12e51a31cdf28024d6bbfc832068c2e8b8015
amelianunes <amnu1000@stud.ek.dk>
2026-03-19 09:16:56 +0100

    ny Order summary print

---
+++ src/PizzaTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaTest {
+
+    @Test
+    void getNumber() {
+        //arrange
+//       Pizza venezia = Pizza.VENEZIA; //behøves ikke arrange, hvis man skriver Pizza-klassen i assert
+        //act
+        //assert
+       // assertEquals(13, Pizza.VENEZIA.getNumber());
+
+    }
+
+    @Test
+    void getName() {
+        //arrange
+        //act
+        //assert
+      //  assertEquals("Dennis",Pizza.DENNIS.getName());
+
+    }
+
+    @Test
+    void getIngredients() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    void getPrice() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    void printMenu() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    void testToString() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    void values() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+
+    @Test
+    void valueOf() {
+        //arrange
+
+        //act
+
+        //assert
+
+    }
+}
