a757c3c57b29871ed0bdf20fd212ff67529e4007
Owen <aramariofficial@gmail.com>
2026-03-20 11:28:43 +0100

    nyt

--- src/Menu.java
+++ src/Menu.java
@@ -2,19 +2,30 @@ import java.util.ArrayList;
 
 public class Menu {
 
-    static ArrayList<Product> menu = new ArrayList<>();
+    private static final ArrayList<Product> menu = new ArrayList<>();
 
     public static void addPizza(Product product) {
         menu.add(product);
+    }
 
+    public static Product getPizzaByNumber(int number) {
+        if (number < 1 || number > menu.size()) {
+            return null;
+        }
+        return menu.get(number - 1);
+    }
+
+    public static int size() {
+        return menu.size();
     }
 
     @Override
     public String toString() {
         String result = "";
 
-        for (Product p : menu) {
-            result += p.getPizza() + " - " + p.getPrice() + " kr\n";
+        for (int i = 0; i < menu.size(); i++) {
+            Product p = menu.get(i);
+            result += (i + 1) + ". " + p.getPizza() + " - " + p.getPrice() + " kr\n";
         }
 
         return result;
